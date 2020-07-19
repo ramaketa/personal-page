ce66c4d04360643f6f396db9d28e6edf96f67fca
Ramazan <ramziknur05@gmail.com>
2020-07-19 20:23:09 +0400

    Refactor header

--- dev-nuriev/src/app/app.module.ts
+++ dev-nuriev/src/app/app.module.ts
@@ -4,24 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import {NbThemeModule, NbLayoutModule, NbActionsModule, NbIconModule} from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NbThemeModule.forRoot({ name: 'default' }),
+    NbThemeModule.forRoot({name: 'default'}),
     NbLayoutModule,
-    NbEvaIconsModule
+    NbEvaIconsModule,
+    NbActionsModule,
+    NbIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
